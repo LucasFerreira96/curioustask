087a5fb151819587cfd3a70312ac09e73e39445c
LucasFerreira96 <lucas-ferreira@outlook.com>
2021-11-19 23:13:11 -0300

    pacotes instalados e layout padrao finalizado

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+//ESTOU DIZENDO PARA O EXPRESS USAR O EJS COMO ENGINE
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+
+    res.render("index");
+
+});
+
+app.get("/perguntar", (req, res) => {
+
+    res.render("perguntar");
+
+});
+
+app.listen(8080, () => {
+    console.log("App rodando");
+});
